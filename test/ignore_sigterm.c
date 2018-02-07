383c2d11fb2e6749ade46d3dfed78d2e48adf052
Frank Hunleth <fhunleth@troodon-software.com>
2018-02-07 14:51:06 -0500

    Simpler blocking of signals

--- test/ignore_sigterm.c
+++ test/ignore_sigterm.c
@@ -3,20 +3,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void ignore_signal(int signum)
-{
-    // Sleeping will keep this process from exiting
-    sleep(1000000);
-}
-
 int main(int argc, char **argv)
 {
-    struct sigaction sa;
-    sa.sa_handler = ignore_signal;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGTERM);
+    sigprocmask(SIG_BLOCK, &mask, NULL);
 
-    sigaction(SIGTERM, &sa, NULL);
     sleep(1000000);
     exit(0);
 }

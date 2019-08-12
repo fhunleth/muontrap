80662c9999eb81c3fd821b4970021428a8cca7a5
Frank Hunleth <fhunleth@troodon-software.com>
2019-08-12 16:10:17 -0400

    Update and format docs

--- lib/muontrap.ex
+++ lib/muontrap.ex
@@ -1,40 +1,87 @@
 defmodule MuonTrap do
   @moduledoc """
-  MuonTrap protects you from lost and out of control processes.
+  MuonTrap protects you from lost and out of control OS processes.
+
+  You can use it as a `System.cmd/3` replacement or to pull OS processes into
+  an Erlang supervision tree via `MuonTrap.Daemon`. Either way, if the Erlang
+  process that runs the command dies, then the OS processes will die as well.
+
+  MuonTrap tries very hard to kill OS processes so that remnants don't hang
+  around the system when your Erlang code thinks they should be gone. MuonTrap
+  can use the Linux kernel's `cgroup` feature to contain the child process and
+  all of its children. From there, you can limit CPU and memory and other
+  resources to the process group.
+
+  MuonTrap does not require `cgroups` but keep in mind that OS processes can
+  escape. It is, however, still an improvement over `System.cmd/3` which does
+  not have a mechanism for dealing it OS processes that do not monitor their
+  stdin for when to close.
+
+  For more information, see the documentation for `MuonTrap.cmd/3` and
+  `MuonTrap.Daemon`
+
+  ## Configuring cgroups
+
+  On most Linux distributions, use `cgcreate` to create a new cgroup.  You can
+  name them almost anything. The command below creates one named `muontrap` for
+  the current user. It supports memory and CPU controls.
+
+  ```sh
+  sudo cgcreate -a $(whoami) -g memory,cpu:muontrap
+  ```
+
+  Nerves systems do not contain `cgcreate` by default. Due to the simpler Linux
+  setup, it may be sufficient to run `File.mkdir_p(cgroup_path)` to create a
+  cgroup. For example:
+
+  ```elixir
+  File.mkdir_p("/sys/fs/cgroup/memory/muontrap")
+  ```
+
+  This creates the cgroup path, `muontrap` under the `memory` controller.  If
+  you do not have the `"/sys/fs/cgroup"` directory, you will need to mount it
+  or update your `erlinit.config` to mount it for you. See a newer official
+  system for an example.
   """
 
   alias MuonTrap.Options
 
   @doc ~S"""
-   Executes a command like `System.cmd/3` via the `muontrap` wrapper.
+  Executes a command like `System.cmd/3` via the `muontrap` wrapper.
 
-   # Options
+  # Options
 
-     * `:cgroup_controllers` - run the command under the specified cgroup controllers. Defaults to `[]`.
-     * `:cgroup_path` - use the specified path for the cgroup
-     * `:cgroup_sets` - set a cgroup controller parameter before running the command
-     * `:delay_to_sigkill` - milliseconds before sending a SIGKILL to a child process if it doesn't exit with a SIGTERM
-     * `:uid` - run the command using the specified uid or username
-     * `:gid` - run the command using the specified gid or group
+    * `:cgroup_controllers` - run the command under the specified cgroup controllers. Defaults to `[]`.
+    * `:cgroup_path` - use the specified path for the cgroup
+    * `:cgroup_sets` - set a cgroup controller parameter before running the command
+    * `:delay_to_sigkill` - milliseconds before sending a SIGKILL to a child process if it doesn't exit with a SIGTERM
+    * `:uid` - run the command using the specified uid or username
+    * `:gid` - run the command using the specified gid or group
 
-   See `System.cmd/3` for additional options.
+  See `System.cmd/3` for additional options.
 
-   # Examples
+  # Examples
 
-   Run a command:
+  Run a command:
 
-      iex> MuonTrap.cmd("echo", ["hello"])
-      {"hello\n", 0}
+  ```elixir
+  iex> MuonTrap.cmd("echo", ["hello"])
+  {"hello\n", 0}
+  ```
 
-   The next examples only run on Linux. To try this out, create new cgroups:
+  The next examples only run on Linux. To try this out, create new cgroups:
 
-      $ sudo cgcreate -a $(whoami) -g memory,cpu:muontrap
+  ```sh
+  sudo cgcreate -a $(whoami) -g memory,cpu:muontrap
+  ```
 
-   Run a command, but limit memory so severely that it doesn't work (for demo
-   purposes, obviously):
+  Run a command, but limit memory so severely that it doesn't work (for demo
+  purposes, obviously):
 
-      iex-donttest> MuonTrap.cmd("echo", ["hello"], cgroup_controllers: ["memory"], cgroup_path: "muontrap/test", cgroup_sets: [{"memory", "memory.limit_in_bytes", "8192"}])
-      {"", 1}
+  ```elixir
+  iex-donttest> MuonTrap.cmd("echo", ["hello"], cgroup_controllers: ["memory"], cgroup_path: "muontrap/test", cgroup_sets: [{"memory", "memory.limit_in_bytes", "8192"}])
+  {"", 1}
+  ```
   """
   @spec cmd(binary(), [binary()], keyword()) ::
           {Collectable.t(), exit_status :: non_neg_integer()}

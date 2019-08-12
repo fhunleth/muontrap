f602565e1e4d45fe96e07b2484dc50966af62392
Frank Hunleth <fhunleth@troodon-software.com>
2019-08-12 13:57:40 -0400

    Fork children in children in "fork_a_lot" helper
    Previously fork_a_lot forked 100 children one level deep. This change
    makes the children fork children to verify that depth of forking isn't
    an issue.
    
    This is not an issue with cgroups but when not using cgroups, children
    get orphaned.

--- test/fork_a_lot.c
+++ test/fork_a_lot.c
@@ -3,15 +3,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char **argv)
+// Fork a tree of children and print out the pids
+
+static void do_fork(int left)
 {
-    for (int i = 0; i < 100; i++) {
+    if (left == 0)
+        return;
+
+    for (int i = 0; i < 2; i++) {
         pid_t pid = fork();
         if (pid == 0) {
             // Child
+            do_fork(left - 1);
             sleep(1000000);
         }
+        printf("%d\n", pid);
+        fflush(stdout);
     }
+
+}
+int main(int argc, char **argv)
+{
+    // Fork a tree of children.
+    // 4 -> this pid + 2 children + 4 grandchildren + 8 great-grandchildren, etc.
+    // for a total of 2^(4+1) - 1 processes
+    do_fork(4);
 
     // parent
     sleep(1000000);

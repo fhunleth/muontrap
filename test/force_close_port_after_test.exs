8f5a3fac3b1f3abcee60e52bbb5b67c05948973c
alanj853 <alanjackson.nuig@gmail.com>
2020-08-21 16:37:35 +0100

    run formatter

--- test/force_close_port_after_test.exs
+++ test/force_close_port_after_test.exs
@@ -1,32 +1,39 @@
 defmodule ForceClosePortAfterTest do
-    use MuonTrapTest.Case
+  use MuonTrapTest.Case
 
-    @temp_output_file "test/force_close_port_after_test_output.txt"
-    @temp_output_text "Hello"
-    @force_close_port_after_seconds 3
-    @expected_output_count @force_close_port_after_seconds
-  
-    test "Test force_close_port_after" do
-      ## Test that command currently not running
-      assert [] == :os.cmd('ps -e | grep force_close_port_after_test')
-      assert File.exists?(@temp_output_file) == false
+  @temp_output_file "test/force_close_port_after_test_output.txt"
+  @temp_output_text "Hello"
+  @force_close_port_after_seconds 3
+  @expected_output_count @force_close_port_after_seconds
 
-      ## run command
-      cmd_output = MuonTrap.cmd("bash", ["test/force_close_port_after_test.sh", @temp_output_file, @temp_output_text], force_close_port_after: :timer.seconds(@force_close_port_after_seconds))
+  test "Test force_close_port_after" do
+    ## Test that command currently not running
+    assert [] == :os.cmd('ps -e | grep force_close_port_after_test')
+    assert File.exists?(@temp_output_file) == false
 
-      ## Test that force_close_port_after actually kicked in
-      assert {"", -11} == cmd_output
-      
-      ## Test that command actually did run by checking if output file is present and contains the correct information
-      assert File.exists?(@temp_output_file) == true
-      assert @expected_output_count == File.read!(@temp_output_file) |> String.split |> Enum.count(fn x -> x == @temp_output_text end)
-      
-      ## Test that command currently not running
-      assert [] == :os.cmd('ps -e | grep force_close_port_after_test')
-      
-      ## tidy up
-      File.rm(@temp_output_file)  
-          
-    end
+    ## run command
+    cmd_output =
+      MuonTrap.cmd(
+        "bash",
+        ["test/force_close_port_after_test.sh", @temp_output_file, @temp_output_text],
+        force_close_port_after: :timer.seconds(@force_close_port_after_seconds)
+      )
+
+    ## Test that force_close_port_after actually kicked in
+    assert {"", -11} == cmd_output
+
+    ## Test that command actually did run by checking if output file is present and contains the correct information
+    assert File.exists?(@temp_output_file) == true
+
+    assert @expected_output_count ==
+             File.read!(@temp_output_file)
+             |> String.split()
+             |> Enum.count(fn x -> x == @temp_output_text end)
+
+    ## Test that command currently not running
+    assert [] == :os.cmd('ps -e | grep force_close_port_after_test')
+
+    ## tidy up
+    File.rm(@temp_output_file)
   end
-  
+end

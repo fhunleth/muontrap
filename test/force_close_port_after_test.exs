f23e0e4bb5bc2c03828c72cfa2b80f0b98ebe870
alanj853 <alanjackson.nuig@gmail.com>
2020-08-29 18:29:07 +0100

    fix first grep command

--- test/force_close_port_after_test.exs
+++ test/force_close_port_after_test.exs
@@ -8,7 +8,7 @@ defmodule ForceClosePortAfterTest do
 
   test "Test force_close_port_after" do
     ## Test that command currently not running
-    assert [] == :os.cmd('ps -e | grep force_close_port_after_test')
+    assert [] == :os.cmd('ps -e | grep [f]orce_close_port_after_test')
     assert File.exists?(@temp_output_file) == false
 
     ## run command

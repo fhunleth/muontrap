3f300d7dcb9d3f04d6be40cca499fa85cc98f8f4
Jon Carstens <jjcarstens@me.com>
2023-06-09 13:33:44 -0600

    Ensure tests are run in CI
    The test helper has functions to require cgroup tools to be installed,
    but was halting `0` if undetected. This caused `mix test` to appear
    to be successful even though no tests were actually run.
    
    This changes the halt to `1` on those conditions so tests actually fail
    when the linux (CI) setup is not correct

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -9,14 +9,14 @@ defmodule MuonTrapTestHelpers do
   def check_cgroup_support() do
     unless System.find_executable("cgget") do
       IO.puts(:stderr, "\nPlease install cgroup-tools so that cgcreate and cgget are available.")
-      System.halt(0)
+      System.halt(1)
     end
 
     unless MuonTrapTest.Case.cpu_cgroup_exists("muontrap_test") and
              MuonTrapTest.Case.memory_cgroup_exists("muontrap_test") do
       IO.puts(:stderr, "\nPlease create the muontrap_test cgroup")
       IO.puts(:stderr, "sudo cgcreate -a $(whoami) -g memory,cpu:muontrap_test")
-      System.halt(0)
+      System.halt(1)
     end
   end
 end

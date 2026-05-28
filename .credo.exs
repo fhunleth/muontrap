7ef5c6dcf308a2658be5ceeae3190b9914621368
Frank Hunleth <fhunleth@troodon-software.com>
2026-05-28 15:56:09 +0200

    Add Credo.Check.Warning.UnsafeToAtom check

--- .credo.exs
+++ .credo.exs
@@ -11,6 +11,7 @@
       checks: [
         {Credo.Check.Refactor.MapInto, false},
         {Credo.Check.Warning.LazyLogging, false},
+        {Credo.Check.Warning.UnsafeToAtom, []},
         {Credo.Check.Readability.LargeNumbers, only_greater_than: 86400},
         {Credo.Check.Readability.ParenthesesOnZeroArityDefs, parens: true},
         {Credo.Check.Readability.Specs, tags: []},
